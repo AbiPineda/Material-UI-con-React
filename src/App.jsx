15f49aca622c2fd21a8555302ec7e0cfb9ee4000
Abigail <pineverdi@gmail.com>
2022-11-03 17:33:21 -0600

    validando usuario

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,46 @@
-import React from "react";
-import { Container } from "@mui/system";
+import React, { useEffect, useState } from "react";
+import { Container } from "@mui/material";
+
 import Searcher from "./components/searchs/search";
+import { getGitHubUser } from './services/users'
 
-const App = () =>{
-    return(
-        <Container sx={{
-            background: 'whitesmoke',
-            width: '80vw',
-            height: '500px',
-            borderRadius:'16px',
-            marginTop: '40px',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center'
-        }}>
-        <Searcher></Searcher>
-        </Container>
+const App = () => {
+const [inputUser, setInputUser] = useState('octocat')
+const [data, setData] = useState([])
+
+useEffect(() =>{
+    (async()=>{
+        try{
+            const res = await getGitHubUser(inputUser)
+            if(inputUser === 'octocat'){
+                localStorage.getItem('octocat', JSON.stringify(res))
+            }
+            if(res.message === 'NOT FOUND'){
+                const octocat = JSON.parse(localStorage.getItem('octocat'))
+                return setData(octocat)
+            }
+            setData(res)
+        }catch(error){
+            console.log('entro aqui');
+        }
+    })()
+},[inputUser])
+
+return(
+      <Container sx={{
+        background: 'whitesmoke',
+        width: '80vw',
+        height: '500px',
+        borderRadius: '16px',
+        marginTop: '40px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }} >
+        <Searcher inputUser={inputUser} setInputUser={setInputUser} />
+      </Container>
     )
-};
-
-export default App;
+  };
+  
+  export default App;
+  
--- src/components/searchs/search.jsx
+++ src/components/searchs/search.jsx
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Stack } from "@mui/system";
 import { IconButton, TextField } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 
-const Searcher = () => {
+const Searcher = (props) => {
+
+  const { setInputUser } = props;
+
+  const [valueInput, setValueInput] = useState('');
+
+   const onSearchValueChange = (event) =>{
+     const inputValue = event.target.value;
+     setValueInput(inputValue);
+   }
+
+  const handleSubmit = () => {
+    setInputUser(valueInput);
+  }
   return (
-    <Stack>
-      <TextField></TextField>
-      <IconButton></IconButton>
+    <Stack 
+    direction = 'row'
+    sx={{
+        marginTop: '30px',
+        width: '90%'
+    }}>
+      <TextField 
+      id="outlined-basic" 
+      label="Github User"
+      placeholder="Buscar Usuario Github"
+      variant="outlined"
+      size="small"
+      value={valueInput}
+      onChange={onSearchValueChange}
+      sx={{
+        width:'90%',
+      }}>
+      </TextField>
+      <IconButton
+     onClick={handleSubmit}
+      size="small"
+      sx={{
+        left:'-45px'
+      }}>
+        <SearchIcon></SearchIcon>
+      </IconButton>
     </Stack>
   );
 };
