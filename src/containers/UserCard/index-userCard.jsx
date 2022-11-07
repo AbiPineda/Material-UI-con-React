0a677931eec70c1281aa13b2cde0344a2b4cce39
AbiPineda <pineverdi@gmail.com>
2022-11-07 11:16:14 -0600

    estilos userCard

--- src/containers/UserCard/index-userCard.jsx
+++ src/containers/UserCard/index-userCard.jsx
@@ -1,4 +1,5 @@
 import { CardMedia, Grid } from "@mui/material";
+import { Stack } from "@mui/system";
 import React from "react";
 
 import PrincipalInformation from "../../components/PrincipalInformation/indexInformation";
@@ -8,7 +9,11 @@ const UserCard = (props) => {
   const { userState } = props;
   const { avatar_url } = userState;
   return (
-    <Grid container spacing={2}>
+    <Grid 
+    container 
+    spacing={2}
+    sx={{ marginTop: '15px' }}
+    >
       {/* se colocara desde donde inicia hasta la posicion 3 */}
       <Grid item xs={3}>
         {/* Aca se mostrara la foto de perfil */}
@@ -16,12 +21,23 @@ const UserCard = (props) => {
         component="img"
         alt="Gibhub User"
         image={avatar_url}
-        >
+        sx={{
+          borderRadius:'50%',
+          marginLeft: '5px'
+        }}>
         </CardMedia>
       </Grid>
       <Grid item xs={9}>
+        <Stack
+        direction="column"   
+        spacing={1}
+        sx={{
+          margin: '30px'
+        }}
+        >
         <PrincipalInformation userState={userState}></PrincipalInformation>
         <Description userState={userState}></Description>
+        </Stack>
       </Grid>
     </Grid>
   );
